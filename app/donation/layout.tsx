618ae2145d7c03720465dce56a2496cece5ff5af
gnomin200408-blip <gnomin200408@gmail.com>
2026-06-17 12:25:02 +0800

    bolooogu

--- app/donation/layout.tsx
+++ app/donation/layout.tsx
@@ -1,9 +1,19 @@
-import { Header } from "../components/Header";
+"use client";
+
+import { useState, useRef } from "react";
+import Header from "../components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [profileOpen, setProfileOpen] = useState(false);
+  const profileRef = useRef<HTMLDivElement>(null); // Хэрэв div дээр бол
+
   return (
     <>
-      <Header />
+      <Header
+        profileRef={profileRef}
+        profileOpen={profileOpen}
+        setProfileOpen={setProfileOpen}
+      />
       {children}
     </>
   );
